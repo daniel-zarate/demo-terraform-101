a0ff973a5b7bb1ab8d89383a828db1b77de16546
daniel-zarate <arkano86@gmail.com>
2019-10-08 11:14:48 -0500

    Cambio en la configuracion del main. Copiando del repor de JP after branch

--- main.tf
+++ main.tf
@@ -1,15 +1,45 @@
+
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "private_key" {}
+variable "public_key" {}
+variable "ami" {}
+variable "subnet_id" {}
+variable "vpc_security_group_id" {}
+variable "identity" {}
+
+variable "num_webs" {
+  default = "1"
+}
+
 provider "aws" {
-  version = "~> 2.30"
-  region  = "us-east-1"
+  region = var.region
 }
 
-module "networking" {
-  source = "./modules/networking/"
+module "server" {
+  source = "./server"
+
+  private_key           = var.private_key
+  public_key            = var.public_key
+  num_webs              = var.num_webs
+  ami                   = var.ami
+  subnet_id             = var.subnet_id
+  vpc_security_group_id = var.vpc_security_group_id
+  identity              = var.identity
 }
 
-module "iam-resources" {
-  source  = "./modules/terraform-aws-workshop-101-iam-resources/"
-  pgp_key = var.PGP_KEY
+output "public_ip" {
+  value = module.server.public_ip
+}
+
+output "public_dns" {
+  value = module.server.public_dns
 }
 
 terraform {
